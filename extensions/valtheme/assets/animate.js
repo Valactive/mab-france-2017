687722ae3b5ae769ac65aa4b2db969479c59d64c
Valéry Frisch <valery@valactive.com>
2018-03-12 21:07:07 +0100

    Update nom de dossier valTheme en valthem

---
+++ extensions/valtheme/assets/animate.js
@@ -0,0 +1,5 @@
+(function($) {
+		$(document).ready(function() {
+			$('#nav li ul').addClass('animated zoomIn');
+		});
+	})(window.jQuery);
---
+++ extensions/valtheme/extension.driver.php
@@ -0,0 +1,23 @@
+<?php
+
+	Class extension_valTheme extends Extension{
+
+		public function getSubscribedDelegates(){
+			return array(
+				array(
+					'page'     => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'appendAssets'
+				),
+			);
+		}
+
+	/*-------------------------------------------------------------------------
+		Delegates:
+	-------------------------------------------------------------------------*/
+
+		public function appendAssets() {
+			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/valtheme/assets/valtheme.css', 'screen', 1000, false);
+		}
+
+	}
