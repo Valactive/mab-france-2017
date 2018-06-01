f7a99ec05f514554583788075b92aa879d57641c
Valéry Frisch <valery@valactive.com>
2018-06-01 19:42:04 +0200

    Update semantic src

--- workspace/semantic/src/themes/default/assets/js/mab.js
+++ workspace/semantic/src/themes/default/assets/js/mab.js
@@ -23,8 +23,7 @@ window.semantic = {
 
   // ready event
   semantic.ready = function() {
-        var $container = $('.main.container');
-        // fix main menu to page on passing
+            // fix main menu to page on passing
             $('.main.menu').visibility({
                 type: 'fixed'
             });
@@ -32,43 +31,75 @@ window.semantic = {
             //     type: 'fixed',
             //     offset: 80
             // });
-        // lazy load images
-            // $('img').visibility({
-            //     type: 'image',
-            //     transition: 'vertical flip in',
-            //     duration: 500
-            // });
-        // show dropdown on hover
-            $('.main.menu .ui.dropdown').dropdown({
-                on: 'hover'
+
+          //lazy load images
+            $('img').visibility({
+                type: 'image',
+                transition: 'vertical flip in',
+                duration: 500
             });
+        // show dropdown on hover
+            // $('.main.menu .ui.dropdown').dropdown({
+            //     on: 'hover'
+            // });
 
             $('.ui.dropdown')
               .dropdown()
             ;
 
+
         // http://gregfranko.com/jquery.tocify.js/
-        $("#tocContents").tocify({
-            context:".main.container",
+        $("#tocContents")
+          .tocify({
+            context:"#content",
             selectors: "h1,h2,h3,h4",
             theme: "none",
-            scrollTo: "65",
-            showAndHide: false
+            scrollTo: "135",
+            showAndHide: false,
+            extendPage: false
         });
 
+        // STICKY MAIN MENU
         $('.ui.sticky')
         .sticky({
-        context: '#content',
-        offset: 65
+          context: '#content',
+          offset: 135,
+          bottomOffset: 20
         });
 
-        // Carte RB
+        // LISTE RB hover
         $('a', '.list-rb').mouseover(function() {
-          var rb = '#' + $(this).data("reserve");
-          console.log(rb);
+          rb = '#' + $(this).data("reserve") + '-on';
           $(rb).hide();
+        }).mouseleave(function(e){
+          $(rb).show();
         });
-  };
 
-  // attach ready event
+        // CARTE RB hover
+        $('path','#reserves-on').mouseenter(function(e){
+          //set active status in rb list
+          rb_on = $(this).data('reserve');
+          rb = rb_on.split('-');
+          rb = rb[0];
+          var listTarget = $(".item[data-reserve='"+rb+"']");
+          listTarget.toggleClass('hovered');
+          $("path[data-reserve='"+rb_on+"']").hide();
+        });
+        $('path').popup(
+          'show',
+          {
+            content: 'the rb',
+            debug: true,
+            verbose: true
+          });
+
+        $('path', '#reserves-off').mouseleave(function(){
+          //rb_id = '#' + $(this).data('reserve') + '-on';
+          $("path[data-reserve='"+rb_on+"']").show();
+          $('a', '.list-rb').removeClass('hovered');
+        });
+        $('path','#reserves-off').popup({
+          content: 'the rb'
+        });
+  };  // attach ready event
 $(document).ready(semantic.ready);
