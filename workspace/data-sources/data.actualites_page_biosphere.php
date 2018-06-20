01960b2e77affc7710ff8ac122bf8f4056ef05bf
Valéry Frisch <valery@valactive.com>
2018-06-20 22:45:23 +0200

    data source for biosphere page news

---
+++ workspace/data-sources/data.actualites_page_biosphere.php
@@ -0,0 +1,90 @@
+<?php
+
+class datasourceactualites_page_biosphere extends SectionDatasource
+{
+    public $dsParamROOTELEMENT = 'actualites-page-biosphere';
+    public $dsParamORDER = 'desc';
+    public $dsParamPAGINATERESULTS = 'yes';
+    public $dsParamLIMIT = '4';
+    public $dsParamSTARTPAGE = '1';
+    public $dsParamREDIRECTONEMPTY = 'no';
+    public $dsParamREDIRECTONFORBIDDEN = 'no';
+    public $dsParamREDIRECTONREQUIRED = 'no';
+    public $dsParamPARAMOUTPUT = array(
+        'logo-odd'
+        );
+    public $dsParamSORT = 'date';
+    public $dsParamHTMLENCODE = 'no';
+    public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
+
+    public $dsParamFILTERS = array(
+        '16' => 'yes',
+        '130' => '{$nom}',
+    );
+
+    public $dsParamINCLUDEDELEMENTS = array(
+        'titre',
+        'texte: formatted',
+        'image-a-la-une',
+        'logo-odd',
+        'date',
+        'type'
+    );
+
+    public function __construct($env = null, $process_params = true)
+    {
+        parent::__construct($env, $process_params);
+        $this->_dependencies = array();
+    }
+
+    public function about()
+    {
+        return array(
+            'name' => 'Actualités page biosphère',
+            'author' => array(
+                'name' => 'Valéry Frisch',
+                'website' => 'http://mab-france-new.localhost',
+                'email' => 'valery.frisch@gmail.com'),
+            'version' => 'Symphony 2.7.2',
+            'release-date' => '2018-06-20T20:31:32+00:00'
+        );
+    }
+
+    public function getSource()
+    {
+        return '6';
+    }
+
+    public function allowEditorToParse()
+    {
+        return true;
+    }
+
+    public function execute(array &$param_pool = null)
+    {
+        $result = new XMLElement($this->dsParamROOTELEMENT);
+
+        try {
+            $result = parent::execute($param_pool);
+        } catch (FrontendPageNotFoundException $e) {
+            // Work around. This ensures the 404 page is displayed and
+            // is not picked up by the default catch() statement below
+            FrontendPageNotFoundExceptionHandler::render($e);
+        } catch (Exception $e) {
+            $result->appendChild(new XMLElement('error',
+                General::wrapInCDATA($e->getMessage() . ' on ' . $e->getLine() . ' of file ' . $e->getFile())
+            ));
+            return $result;
+        }
+
+        if ($this->_force_empty_result) {
+            $result = $this->emptyXMLSet();
+        }
+
+        if ($this->_negate_result) {
+            $result = $this->negateXMLSet();
+        }
+
+        return $result;
+    }
+}
